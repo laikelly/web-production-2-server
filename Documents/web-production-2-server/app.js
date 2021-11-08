37f728a48334786afe8568b8b4469995f109e0f5
laikelly <lai.kellyy@gmail.com>
2021-11-07 20:53:37 -0500

    undo

--- Documents/web-production-2-server/app.js
+++ Documents/web-production-2-server/app.js
@@ -1,72 +1,38 @@
-var express = require('express');
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
 
-var app = express();
+const app = express()
 
-// set up handlebars view engine
-var handlebars = require('express-handlebars').create({
-    defaultLayout:'main'
-});
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+// allow our app to access images and other content in the public folder
+app.use(express.static(__dirname + '/public'))
 
-app.set('port', process.env.PORT || 3000);
+// tell our app to render views from the views folder
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main'
+}))
 
-app.use(express.static(__dirname + '/public'));
+// use handlebars for views
+app.set('view engine', 'handlebars')
 
-let listOfWorks = [{title: 'My Very Special Work', link: 'https://zombo.com', image: '/img/panda.jpg'}]
-// if you want to add your work to a partial
-// middleware to add list data to context
-app.use(function(req, res, next){
-	if(!res.locals.partials) res.locals.partials = {};
-  // 	res.locals.partials.listOfWorks = listOfWorks;
- 	next();
-});
+const port = process.env.PORT || 3000
 
-
-let moreInfo = ['My favorite programming language is Python', 'My favorite food is burgers', "I was once interviewed by the New York Times About Tinder"]
-
-app.get('/', function(req, res) {
-  res.render('home');
-});
-
-app.get('/about', function(req,res){
-	res.render('about', {
-		moreInfo: moreInfo
-	});
-});
-
-
-app.get('/my-work', function(req, res) {
-  res.render('works', {
-    works: listOfWorks
-  })
+app.get('/', (req, res) => {
+  console.log('got index page')
+  res.render('index')
 })
 
-app.get('/works/:number', function(req, res) {
- let pageIndex = parseInt(req.params.number)
-  console.log('page index: ', pageIndex)
-  let nextPage = pageIndex + 1< listOfWorks.length ? pageIndex + 1 : false
-  console.log('next page: ', nextPage)
-  res.render('work', {
-    work: listOfWorks[pageIndex],
-    nextPage: nextPage
-  })
+app.use((req, res) => {
+  res.type('text/plain')
+  res.status
+(404)
+  res.send('404 - not found')
 })
 
-// 404 catch-all handler (middleware)
-app.use(function(req, res, next){
-	res.status(404);
-	res.render('404');
-});
+app.use((err, req, res, next) => {
+  console.log('error:', err.message)
+  res.type('text/plain')
+  res.status(500)
+  res.send('500 - server error')
+})
 
-// 500 error handler (middleware)
-app.use(function(err, req, res, next){
-	console.error(err.stack);
-	res.status(500);
-	res.render('500');
-});
-
-app.listen(app.get('port'), function(){
-  console.log( 'Express started on http://localhost:' +
-    app.get('port') + '; press Ctrl-C to terminate.' );
-});
+app.listen(port, () => console.log(`Express started on http://localhost:${port} ` + `press Ctrl-C to terminate`))
